0128e6556c459d8e59769e988a1440f68e9f9a01
Minji <jminji98@gmail.com>
2020-04-06 17:03:54 +0900

    Ad8
    Co-Authored-By: nahye03 <nahye03@users.noreply.github.com>
    Co-Authored-By: hannayeoniee <hannayeoniee@users.noreply.github.com>

---
+++ ui수정1/t2+그래프 eeg수정/Ad8.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace t2
+{
+    /// <summary>
+    /// Ad8.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Ad8 : Window
+    {
+        public Ad8()
+        {
+            InitializeComponent();
+            ad8.Play();
+        }
+        private void Element_MediaOpened(object sender, EventArgs e)
+        {
+            timelineSlider.Maximum = ad8.NaturalDuration.TimeSpan.TotalMilliseconds;
+        }
+
+        // When the media playback is finished. Stop() the media to seek to media start.
+        private void Element_MediaEnded(object sender, EventArgs e)
+        {
+            ad8.Stop();
+            Expermode3 mode3 = new Expermode3();
+            App.Current.MainWindow = mode3;
+            mode3.Show();
+            this.Close();
+        }
+    }
+}
