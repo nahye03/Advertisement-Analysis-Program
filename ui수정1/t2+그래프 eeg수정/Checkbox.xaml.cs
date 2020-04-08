f0ca9eef2f8f3309c6d7bb38a5d7cd2fc61db5fd
Minji <jminji98@gmail.com>
2020-04-08 14:41:00 +0900

    Checkbox
    Co-Authored-By: nahye03 <nahye03@users.noreply.github.com>
    Co-Authored-By: hannayeoniee <hannayeoniee@users.noreply.github.com>

---
+++ ui수정1/t2+그래프 eeg수정/Checkbox.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace t2
+{
+    /// <summary>
+    /// Checkbox.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Checkbox : Window
+    {
+        public Checkbox()
+        {
+            InitializeComponent();
+        }
+
+        private void commercial_checkbox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Analysis um = new Analysis();
+            App.Current.MainWindow = um;
+            um.Show();
+            this.Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Analysis3 ana1 = new Analysis3();
+            App.Current.MainWindow = ana1;
+            ana1.Show();
+            this.Close();
+        }
+    }
+}
