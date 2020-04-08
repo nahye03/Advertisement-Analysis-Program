681266a3bc3e2da3d6d84dbe9dacc940a872b043
Minji <jminji98@gmail.com>
2020-04-08 14:40:47 +0900

    Create Analysis3.xaml.cs
    Co-Authored-By: nahye03 <nahye03@users.noreply.github.com>
    Co-Authored-By: hannayeoniee <hannayeoniee@users.noreply.github.com>

---
+++ ui수정1/t2+그래프 eeg수정/Analysis3.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace t2
+{
+    /// <summary>
+    /// Analysis3.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Analysis3 : Window
+    {
+        public Analysis3()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow ma = new MainWindow();
+            App.Current.MainWindow = ma;
+            ma.Show();
+            this.Close();
+        }
+    }
+}
