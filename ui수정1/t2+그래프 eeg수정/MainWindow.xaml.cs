575a559f3e275fa38217c9b9accd8bddc32e6abb
Minji <jminji98@gmail.com>
2020-04-29 02:12:27 +0900

    MainWindow
    Co-Authored-By: hannayeoniee <hannayeoniee@users.noreply.github.com>
    Co-Authored-By: nahye03 <nahye03@users.noreply.github.com>

---
+++ ui수정1/t2+그래프 eeg수정/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace t2
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void expermode_Click(object sender, RoutedEventArgs e)
+        {
+
+            Expermode0 window1 = new Expermode0();
+            App.Current.MainWindow = window1;
+            window1.Show();
+            this.Close();
+        }
+
+        private void usermode_Click(object sender, RoutedEventArgs e)
+        {
+            Usermode1 user1 = new Usermode1();
+            App.Current.MainWindow = user1;
+            user1.Show();
+            this.Close();
+        }
+    }
+}
