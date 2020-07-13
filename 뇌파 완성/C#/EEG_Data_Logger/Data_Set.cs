b458d8d9fa5388f2ace0f765d74453b3cf90c8a3
minji-o-j <jminji98@gmail.com>
2020-07-13 15:14:02 +0900

    eeg
    Co-Authored-By: nahye03 <54797864+nahye03@users.noreply.github.com>
    Co-Authored-By: hannayeoniee <33839093+hannayeoniee@users.noreply.github.com>

---
+++ 뇌파 완성/C#/EEG_Data_Logger/Data_Set.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EEG_Data_Logger
+{
+    class Data_Set
+    {
+        public double[,] series;
+        public Data_Set()
+        {
+            series = new double[26, 128];
+        }
+        public void AddData(int i, int j, double data)
+        {
+            series[i, j] = data;
+        }
+    }
+}
---
+++ 뇌파 완성/C#/EEG_Data_Logger/EEG_Data_Analysis.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EEG_Data_Logger
+{
+    class EEG_Data_Analysis
+    {
+        double[][] rawEEG;
+
+        public EEG_Data_Analysis() 
+        {
+            rawEEG = new double[14][];
+        }
+
+        public double[][] GetRawEEG(double[][] src) //채널별 뇌파로 전체 뇌파
+        {
+            Array.Copy(src, rawEEG, 128*14);
+            return rawEEG;
+        }
+
+        public double GetP300(double[] channel) //p300선택
+        {
+            double[] src = new double[32];
+            Array.Copy(channel, 32, src, 0, 32);
+
+            PeakDetection peak = new PeakDetection(channel);
+
+            var p300 = peak.GetValue();
+            return p300;
+        }
+    }
+}
