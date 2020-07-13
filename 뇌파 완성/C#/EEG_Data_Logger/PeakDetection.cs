28257a24c16848efe14f960b9b3fd6266e5cb39c
minji-o-j <jminji98@gmail.com>
2020-07-13 15:13:33 +0900

    eeg
    Co-Authored-By: nahye03 <54797864+nahye03@users.noreply.github.com>
    Co-Authored-By: hannayeoniee <33839093+hannayeoniee@users.noreply.github.com>

---
+++ 뇌파 완성/C#/EEG_Data_Logger/PeakDetection.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EEG_Data_Logger
+{
+    class PeakDetection
+    {
+        private int lastindex = 0;
+        private double lastval;
+
+        public PeakDetection(double[] data)
+        {
+            Excute(data);
+        }
+
+        public void Excute(double[] result)
+        {
+            lastval = result[0];
+            bool dirpos = true;
+            for (int i = 0; i < result.Length; i++)
+            {
+                var currentval = result[i];
+                if (dirpos && currentval < lastval)
+                {
+                    //Console.WriteLine(string.Format("i: {0} value: {1}", lastindex, result[lastindex]));
+                    dirpos = false;
+                }
+                if (currentval >= lastval)
+                    dirpos = true;
+
+                lastval = currentval;
+                lastindex = i;
+            }
+        }
+
+        public int GetIndex()
+        {
+            return lastindex;
+        }
+
+        public double GetValue()
+        {
+            return lastval;
+        }
+    }
+}
